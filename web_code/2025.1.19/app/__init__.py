0ba1c60c496be8ff847085f37e046587ea67ea4b
Alchemist-iridium <103753280+Alchemist-iridium@users.noreply.github.com>
2025-01-19 18:18:35 -0500

    Add files via upload
    add a missing button
    build the item display section
    build the functions that support the one-to-many relationship of product and production round (active stage, archive detail and dialog, button display)
    
    Enough to start on the delivery order system, which is triggered by the examine status

---
+++ web_code/2025.1.19/app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from app.extensions import db, celery, init_extensions
+import os
+
+def create_app():
+    # Create the Flask app
+    app = Flask(__name__)
+    app.secret_key = 'supersecretkey'
+    app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'static/uploads')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///art_platform.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # Initialize extensions
+    init_extensions(app)
+
+    # Register the blueprints here
+    from app.routes.user_routes import user_interface
+    from app.routes.admin_routes import admin_interface
+
+    app.register_blueprint(user_interface, url_prefix='/')
+    app.register_blueprint(admin_interface, url_prefix='/admin')
+
+    return app
